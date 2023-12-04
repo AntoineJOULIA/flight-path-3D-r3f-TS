c5072b775830260f30aa87634f242cc61d8b7695
Antoine Joulia <antoine.joulia@onera.fr>
2023-12-04 17:29:24 +0100

    refactor(trajectory): 1 single component for each line, static, animated

--- src/components/LineTrajectory.tsx
+++ src/components/LineTrajectory.tsx
@@ -1,13 +1,41 @@
-import { LineCityPairTrajectory } from "./LineCityPairTrajectory";
-import { LineFlightTrajectory } from "./LineFlightTrajectory";
+import { Line } from "@react-three/drei";
+import { Vector3 } from "three";
+import { CityPair, Flight } from "../types/types";
+import { CURVE_SEGMENTS } from "../utils/constants";
+import { createCurveFromFlight, createSplineFromCityPair } from "../utils/geom";
 import { isCityPairInput, TrajectoryInput } from "./Trajectory";
 
-type Props = { input: TrajectoryInput };
-function LineTrajectory(props: Props) {
-  if (isCityPairInput(props.input)) {
-    return <LineCityPairTrajectory cityPair={props.input} />;
+function LineTrajectory({ input }: { input: TrajectoryInput }) {
+  if (isCityPairInput(input)) {
+    return <LineCityPairTrajectory cityPair={input} />;
   }
-  return <LineFlightTrajectory flight={props.input} />;
+  return <LineFlightTrajectory flight={input} />;
+}
+
+function LineCityPairTrajectory({ cityPair }: { cityPair: CityPair }) {
+  const { spline } = createSplineFromCityPair(cityPair);
+  const points: Vector3[] = [];
+  const vertices = spline.getPoints(CURVE_SEGMENTS - 1);
+
+  for (let i = 0; i < vertices.length; i++) {
+    const vertex = vertices[i];
+    points.push(vertex);
+  }
+
+  return <Line points={points} color="skyblue" linewidth={1}></Line>;
+}
+
+function LineFlightTrajectory({ flight }: { flight: Flight }) {
+  const curve = createCurveFromFlight(flight);
+  const points: Vector3[] = [];
+  const vertices = curve.getPoints(CURVE_SEGMENTS - 1);
+
+  for (let i = 0; i < vertices.length; i++) {
+    const vertex = vertices[i];
+    points.push(vertex);
+  }
+
+  return <Line points={points} color="skyblue" linewidth={1}></Line>;
 }
 
 export { LineTrajectory };
--- src/components/StaticTrajectory.tsx
+++ src/components/StaticTrajectory.tsx
@@ -1,13 +1,62 @@
-import { StaticCityPairTrajectory } from "./StaticCityPairTrajectory";
-import { StaticFlightTrajectory } from "./StaticFlightTrajectory";
+import { shaderMaterial, Tube } from "@react-three/drei";
+import { extend } from "@react-three/fiber";
+import { Color } from "three";
+import { CityPair, Flight } from "../types/types";
+import { createCurveFromFlight, createSplineFromCityPair } from "../utils/geom";
 import { isCityPairInput, TrajectoryInput } from "./Trajectory";
 
-type Props = { input: TrajectoryInput };
-function StaticTrajectory(props: Props) {
-  if (isCityPairInput(props.input)) {
-    return <StaticCityPairTrajectory cityPair={props.input} />;
+function StaticTrajectory({ input }: { input: TrajectoryInput }) {
+  if (isCityPairInput(input)) {
+    return <StaticCityPairTrajectory cityPair={input} />;
   }
-  return <StaticFlightTrajectory flight={props.input} />;
+  return <StaticFlightTrajectory flight={input} />;
 }
 
+const FadingMaterial = shaderMaterial(
+  {
+    uTime: 0,
+    uColor: new Color(0.1, 0.5, 0.8),
+  },
+  `varying vec2 vUv;
+  
+ void main() {
+	vUv = uv;
+	gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+ } `,
+  `varying vec2 vUv;
+  uniform float uTime;
+  uniform vec3 uColor;
+
+  void main() {
+	float alpha = vUv.x;
+	vec4 color = vec4(uColor, alpha);
+	gl_FragColor = color;
+  }
+  `
+);
+extend({ FadingMaterial });
+
+function StaticCityPairTrajectory({ cityPair }: { cityPair: CityPair }) {
+  const { spline } = createSplineFromCityPair(cityPair);
+
+  return (
+    <Tube args={[spline, 200, 1, 8]}>
+      {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
+      {/* @ts-ignore */}
+      <fadingMaterial transparent={true} uColor={new Color(0.1, 0.5, 0.8)} />
+    </Tube>
+  );
+}
+
+function StaticFlightTrajectory({ flight }: { flight: Flight }) {
+  const curve = createCurveFromFlight(flight);
+
+  return (
+    <Tube args={[curve, 200, 0.1, 8]}>
+      {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
+      {/* @ts-ignore */}
+      <fadingMaterial transparent={true} uColor={new Color(0.1, 0.5, 0.8)} />
+    </Tube>
+  );
+}
 export { StaticTrajectory };
