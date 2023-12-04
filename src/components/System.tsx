f132243856f1a9369bf8c44ee4d90d2ec17c48b4
Antoine Joulia <antoine.joulia@onera.fr>
2023-12-04 12:03:45 +0100

    Animated trajectory with Flight as input

--- src/components/System.tsx
+++ src/components/System.tsx
@@ -4,8 +4,8 @@ import { Group } from "three";
 import { cityPairs } from "../utils/cityPairs";
 import { flight } from "../utils/flight";
 import { AnimatedCityPairTrajectory } from "./AnimatedCityPairTrajectory";
+import { AnimatedFlightTrajectory } from "./AnimatedFlightTrajectory";
 import { Earth } from "./Earth";
-import { TubeFlightTrajectory } from "./TubeFlightTrajectory";
 
 function System() {
   // The exclamation mark is a non-null assertion that will let TS know that ref.current is defined when we access it in effects.
@@ -18,7 +18,7 @@ function System() {
   return (
     <group ref={ref}>
       <Earth />
-      <TubeFlightTrajectory flight={flight} />
+      <AnimatedFlightTrajectory flight={flight} />
       {cityPairs.map((cityPair) => (
         <AnimatedCityPairTrajectory key={`${cityPair[0] - cityPair[1]}`} cityPair={cityPair} />
       ))}
